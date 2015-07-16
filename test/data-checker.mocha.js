53c166abd0690e9ab84f22ea6cce5c48c1a6f788
Aymeric Lavit d'Hautefort <alavit-d@student.42.fr>
2015-07-16 17:15:26 +0200

    added new way to calculate deviation using mean vs EMA

--- test/data-checker.mocha.js
+++ test/data-checker.mocha.js
@@ -70,7 +70,6 @@ describe('Smart Data Checker', function() {
 
     var dtCheck = new dataChecker({
       timer : 100,
-      dev   : 1,
       callback : function() {
         done(new Error('should not be called'));
       },
@@ -89,6 +88,38 @@ describe('Smart Data Checker', function() {
     }, 100);
 
     setTimeout(function() {
+      // Success!
+      done();
+    }, 15000);
+  });
+
+ it('Scenario 1 - calculated with mean over 30s', function(done) {
+
+    var dtCheck = new dataChecker({
+      timer : 100,
+      dev   : 1,
+      callback : function() {
+        done(new Error('should not be called'));
+      },
+      refresh : function() {
+        return current_value;
+      }
+    });
+
+    var interval = setInterval(function() {
+      current_value++;
+
+      if (current_value % 30 == 0)
+        current_value += 500;
+      else if (current_value > 500)
+        current_value -= 500;
+    }, 100);
+
+
+    setTimeout(function() {
+      clearInterval(interval);
+      dtCheck.stop();
+
       // Success!
       done();
     }, 15000);
