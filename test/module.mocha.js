558c472b791a5851da5962c62fb477002b055bde
Unitech <strzelewicz.alexandre@gmail.com>
2015-07-24 18:23:16 +0200

    add test - add specific module conf variable in module_conf

--- test/module.mocha.js
+++ test/module.mocha.js
@@ -2,10 +2,18 @@
 var pmx = require('..');
 var should = require('should');
 
-function fork() {
+function forkWithoutEnv() {
   var app = require('child_process').fork(__dirname + '/fixtures/module/module.fixture.js', [], {
     env : {
-      'module' : '{ "option1" : "value1", "option2" : "value2" }'
+    }
+  });
+  return app;
+}
+
+function forkWithSpecificVar() {
+  var app = require('child_process').fork(__dirname + '/fixtures/module/module.fixture.js', [], {
+    env : {
+      'module' : '{ "option1" : "value1", "option2" : "value2", "initial" : "over" }'
     }
   });
   return app;
@@ -17,7 +25,40 @@ describe('PMX module', function() {
 
   it('should emit a new action', function(done) {
     // 1 - It should emit an action
-    app = fork();
+    app = forkWithoutEnv();
+
+    app.once('message', function(dt) {
+
+      /**
+       * Right event sent
+       */
+      dt.type.should.eql('axm:option:configuration');
+
+      /**
+       * Options set
+       */
+      dt.data.show_module_meta.should.exists;
+      dt.data.description.should.eql('comment');
+      dt.data.module_version.should.eql('1.0.0');
+      dt.data.module_name.should.eql('module');
+
+      /**
+       * Configuration succesfully passed
+       */
+      dt.data.initial.should.eql('init-val');
+
+      /**
+       * Should configuration variable be mirrored into module_conf
+       * attribute (for keymetrics purposes)
+       */
+      dt.data.module_conf.initial.should.eql('init-val');
+      done();
+    });
+  });
+
+  it('should emit a new action', function(done) {
+    // 1 - It should emit an action
+    app = forkWithSpecificVar();
 
     app.once('message', function(dt) {
 
@@ -39,6 +80,15 @@ describe('PMX module', function() {
        */
       dt.data.option1.should.eql('value1');
       dt.data.option2.should.eql('value2');
+      dt.data.initial.should.eql('over');
+
+      /**
+       * Should configuration variable be mirrored into module_conf
+       * attribute (for keymetrics purposes)
+       */
+      dt.data.module_conf.option1.should.eql('value1');
+      dt.data.module_conf.option2.should.eql('value2');
+      dt.data.module_conf.initial.should.eql('over');
       done();
     });
   });
